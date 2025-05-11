e475279e0cae9a1eb77eff48f48ae1c9c12f318f
lorenzo-milanese <lori.mila@icloud.com>
2025-05-12 01:38:09 +0200

    Primi 1000 numeri romani close#4

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,11 +6,11 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
-        if (number > 10 || number <= 0) {
-            return "errore: numeri solo tra 1 e 10";
+        if (number > 1000 || number <= 0) {
+            return "errore: numeri solo tra 1 e 1000";
         }
-        int[] values = {10, 9, 5, 4, 1 };
-        String[] romanLetters = {"X", "IX", "V", "IV", "I" };
+        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+        String[] romanLetters = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
         StringBuilder roman = new StringBuilder();
         for (int i = 0; i < values.length; i++) {
             while (number >= values[i]) {
--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -3,54 +3,89 @@
 // Edoardo Granziero 2110996
 ////////////////////////////////////////////////////////////////////
 
-package it.unipd.mtss; 
-import java.util.HashMap;  
+package it.unipd.mtss;
+import java.util.HashMap;
 
-public class RomanPrinter{ 
-    public static String print(int num){ 
+public class RomanPrinter{
+    public static String print(int num){
         return printAsciiArt(IntegerToRoman.convert(num));
-    } 
- 
-    private static String printAsciiArt(String romanNumber) { 
-        if(romanNumber == "errore: numeri solo tra 1 e 10"){
+    }
+
+    private static String printAsciiArt(String romanNumber) {
+        if(romanNumber == "errore: numeri solo tra 1 e 1000"){
             System.out.println(romanNumber);
             return romanNumber;
         }
-            
-        HashMap<String, String[]> AsciiArt = new HashMap<String, String[]>(); 
-        AsciiArt.put("I", new String[] {" _____ ",
-                                        "|_   _|",
-                                        "  | |  ",
-                                        "  | |  ",
-                                        " _| |_ ",
-                                        "|_____|"});
-        AsciiArt.put("V", new String[] {"__      __",
-                                        "\\ \\    / /",
-                                        " \\ \\  / / ",
-                                        "  \\ \\/ /  ",
-                                        "   \\  /   ",
-                                        "    \\/    "});
-        AsciiArt.put("X", new String[] {"__   __",
-                                        "\\ \\ / /",
-                                        " \\ V / ",
-                                        "  > <  ",
-                                        " / . \\ ",
-                                        "/_/ \\_\\"});
-            
-        int Len = romanNumber.length(); 
+
+        HashMap<String, String[]> AsciiArt = new HashMap<String, String[]>();
+        AsciiArt.put("I", new String[]{
+                " _____ ",
+                "|_   _|",
+                "  | |  ",
+                "  | |  ",
+                " _| |_ ",
+                "|_____|"});
+        AsciiArt.put("V", new String[] {
+                "__        __",
+                "\\ \\    / /",
+                " \\ \\  / / ",
+                "  \\ \\/ /  ",
+                "   \\  /   ",
+                "    \\/    "});
+        AsciiArt.put("X", new String[] {
+                "__   __",
+                "\\ \\ / /",
+                " \\ V / ",
+                "  > <  ",
+                " / . \\ ",
+                "/_/ \\_\\"});
+
+        AsciiArt.put("L", new String[] {
+                " _      ",
+                "| |     ",
+                "| |     ",
+                "| |     ",
+                "| |____ ",
+                "|______|"});
+
+        AsciiArt.put("C", new String[] {
+                "  _____ ",
+                " / ____|",
+                "| |     ",
+                "| |     ",
+                "| |____ ",
+                " \\_____|"});
+
+        AsciiArt.put("D", new String[] {
+                " _____  ",
+                "|  __ \\ ",
+                "| |  | |",
+                "| |  | |",
+                "| |__| |",
+                "|_____/ "});
+
+        AsciiArt.put("M", new String[] {
+                " __  __ ",
+                "|  \\/  |",
+                "| \\  / |",
+                "| |\\/| |",
+                "| |  | |",
+                "|_|  |_|"});
+
+        int RNLength = romanNumber.length();
         String ris ="";
-        
-        for(int i = 0; i < 6; i++){ 
-            for(int j = 0; j < Len; j++){ 
-                String tmp = AsciiArt.get(String.valueOf(romanNumber.charAt(j)))[i];                 
-                System.out.print(tmp); 
-                System.out.print("  "); 
+
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < RNLength; j++){
+                String tmp = AsciiArt.get(String.valueOf(romanNumber.charAt(j)))[i];
+                System.out.print(tmp);
+                System.out.print("  ");
                 ris += tmp + "  ";
-            } 
+            }
             System.out.print("\n");
             ris += "\n";
-        } 
+        }
         return ris;
-    } 
-  
- }
+    }
+
+}
